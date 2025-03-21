b8274fbd96c699b1b89fd99fca5a9e69595fded3
Tegar Aditya <tegaraditya53@gmail.com>
2025-03-21 15:03:11 +0700

    add config package to manage application configuration

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     int
+	DBUser     string
+	DBPassword string
+	DBName     string
+	Port       int
+}
+
+func LoadConfig() Config {
+
+	dbPort, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+
+	return Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     dbPort,
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		Port:       port,
+	}
+}
