1cead137f22b0f82d29a6153bca21b21d66f4f3c
Tegar Aditya <tegaraditya53@gmail.com>
2025-03-22 09:38:48 +0700

    initialize application with environment configuration and database connection

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"bupin-qr-gen-go/config"
+	"bupin-qr-gen-go/database"
+	"bupin-qr-gen-go/router"
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	cfg := config.LoadConfig()
+
+	database.InitDB()
+
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Fiber!")
-	})
+	router.SetupRoutes(app)
 
-	app.Listen(":3000")
+	log.Printf("Starting server on port %d", cfg.Port)
+	if err := app.Listen(fmt.Sprintf(":%d", cfg.Port)); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
