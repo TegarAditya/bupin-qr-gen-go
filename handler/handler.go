9b4fc9de7211b916aa7660f85040aa6563c25187
Tegar Aditya <tegaraditya53@gmail.com>
2025-03-22 09:38:43 +0700

    add handler package with GetQRCode function for QR code retrieval

---
+++ handler/handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"bupin-qr-gen-go/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetQRCode(c *fiber.Ctx) error {
+	id := c.Params("id")
+	value, err := utils.GetInfoVID(id)
+	if err != nil {
+		return c.Status(404).SendString("String not found")
+	}
+
+	filename := utils.GetFileName(value)
+
+	url := "https://buku.bupin.id/?" + id
+
+	qrCode, err := utils.GenerateQRCode(url)
+	if err != nil {
+		return c.Status(500).SendString("Failed to generate QR code")
+	}
+
+	c.Set("Content-Type", "image/png")
+	c.Set("Content-Disposition", "attachment; filename="+filename+".png")
+	return c.Send(qrCode)
+}
