43ea28dfad5b5104f8e18eed6d79a60943866b85
Tegar Aditya <tegaraditya53@gmail.com>
2025-03-24 10:40:54 +0700

    feat:add support for UJN codes

--- handler/handler.go
+++ handler/handler.go
@@ -2,19 +2,30 @@ package handler
 
 import (
 	"bupin-qr-gen-go/utils"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetQRCode(c *fiber.Ctx) error {
 	id := c.Params("id")
-	value, err := utils.GetInfoVID(id)
+
+	var (
+		value any
+		err   error
+	)
+
+	if strings.HasPrefix(id, "UJN") {
+		value, err = utils.GetInfoUJN(id)
+	} else {
+		value, err = utils.GetInfoVID(id)
+	}
+
 	if err != nil {
 		return c.Status(404).SendString("String not found")
 	}
 
 	filename := utils.GetFileName(value)
-
 	url := "https://buku.bupin.id/?" + id
 
 	qrCode, err := utils.GenerateQRCode(url)
