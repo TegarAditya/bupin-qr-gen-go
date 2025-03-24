a02042ef4d8afda86f3997aecd8f345845570b5a
Tegar Aditya <tegaraditya53@gmail.com>
2025-03-24 10:39:54 +0700

    refactor: update database struct field names for consistency and clarity

--- model/model.go
+++ model/model.go
@@ -1,16 +1,8 @@
 package model
 
-import (
-	"database/sql"
-)
-
 type InfoUJN struct {
-	KodeQRUjian string `db:"kodeQR"`
+	KodeQR      string `db:"kodeQRUjian"`
 	IDUjian     int    `db:"idUjian"`
-	IDJenjang   int    `db:"idJenjang"`
-	IDKelas     int    `db:"idKelas"`
-	IDMapel     int    `db:"idMapel"`
-	IDBab       int    `db:"idBab"`
 	NamaJenjang string `db:"namaJenjang"`
 	NamaKelas   string `db:"namaKelas"`
 	NamaBab     string `db:"namaBab"`
@@ -18,19 +10,10 @@ type InfoUJN struct {
 }
 
 type InfoVID struct {
-	KodeQR      string         `db:"kodeQR"`
-	NamaJenjang string         `db:"namaJenjang"`
-	NamaKelas   string         `db:"namaKelas"`
-	NamaMapel   string         `db:"namaMapel"`
-	NamaBab     string         `db:"namaBab"`
-	NamaSubBab  string         `db:"namaSubBab"`
-	IDKelas     int            `db:"idKelas"`
-	IDMapel     int            `db:"idMapel"`
-	IDBab       int            `db:"idBab"`
-	IDSubBab    int            `db:"idSubBab"`
-	LinkVideo   string         `db:"linkVideo"`
-	Ytid        string         `db:"ytid"`
-	LinkDMP     sql.NullString `db:"linkDmp"`
-	YtidDMP     sql.NullString `db:"ytidDmp"`
-	TP          string         `db:"tp"`
+	KodeQR      string `db:"kodeQR"`
+	NamaJenjang string `db:"namaJenjang"`
+	NamaKelas   string `db:"namaKelas"`
+	NamaMapel   string `db:"namaMapel"`
+	NamaBab     string `db:"namaBab"`
+	NamaSubBab  string `db:"namaSubBab"`
 }
