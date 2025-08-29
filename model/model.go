e34c7665d82a503da55b8c4358ae772c504a0390
Tegar Aditya <tegaraditya53@gmail.com>
2025-08-29 08:36:21 +0700

    fix: reorder fields in InfoUJN struct

--- model/model.go
+++ model/model.go
@@ -2,11 +2,11 @@ package model
 
 type InfoUJN struct {
 	KodeQR      string `db:"kodeQRUjian"`
-	IDUjian     int    `db:"idUjian"`
 	NamaJenjang string `db:"namaJenjang"`
 	NamaKelas   string `db:"namaKelas"`
-	NamaBab     string `db:"namaBab"`
 	NamaMapel   string `db:"namaMapel"`
+	NamaBab     string `db:"namaBab"`
+	IDUjian     int    `db:"idUjian"`
 }
 
 type InfoVID struct {
